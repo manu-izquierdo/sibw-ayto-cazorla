172022f34ba7b3a54a65bbd396d69c14da6662a6
Manuizquierdoizq <manueljesusizquierdoizquierdo@gmail.com>
2026-04-05 16:38:58 +0200

    add: primera integracion de consultas en la base de datos

--- www/noticia.php
+++ www/noticia.php
@@ -4,7 +4,22 @@
     $loader = new \Twig\Loader\FilesystemLoader('templates');
     $twig = new \Twig\Environment($loader);
 
-    // 3. (Futuro) Aquí harás las consultas SQL a la base de datos
 
-    echo $twig->render('noticia.html.twig', []);
+    $mysqli = new mysqli("lamp-mysql8", "root", "tiger", "sibw");
+    
+    // 2. Validación del parámetro GET
+    $idEvt = isset($_GET['id']) ? (int)$_GET['id'] : 1;
+
+    // 3. Consulta de la noticia
+    $res = $mysqli->query("SELECT * FROM noticias WHERE id = $idEvt");
+    $noticia = $res->fetch_assoc();
+
+    // 4. Consulta de imágenes relacionadas
+    $resImgs = $mysqli->query("SELECT * FROM imagenes WHERE noticia_id = $idEvt");
+    $imagenes = $resImgs->fetch_all(MYSQLI_ASSOC);
+
+    echo $twig->render('noticia.html.twig', [
+        'noticia' => $noticia,
+        'imagenes' => $imagenes
+    ]);
 ?>
