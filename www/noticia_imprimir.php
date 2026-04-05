6460be608e7d638fdcc3f2198110a3425420f9b6
Manuizquierdoizq <manueljesusizquierdoizquierdo@gmail.com>
2026-04-05 22:30:13 +0200

    feat(print): implementar carga dinámica de imágenes en vista de impresión

--- www/noticia_imprimir.php
+++ www/noticia_imprimir.php
@@ -4,7 +4,32 @@
     $loader = new \Twig\Loader\FilesystemLoader('templates');
     $twig = new \Twig\Environment($loader);
 
-    // 3. (Futuro) Aquí harás las consultas SQL a la base de datos
+    $host = "lamp-mysql8";
+    $db   = "sibw";
+    $user = "manu_sibw";
+    $pass = "practica3";
 
-    echo $twig->render('noticia_imprimir.html.twig', []);
+    $mysqli = new mysqli($host, $user, $pass, $db);
+
+    if ($mysqli->connect_error) {
+        die("Error de conexión: " . $mysqli->connect_error);
+    }
+
+    // Validación del parámetro GET
+    $id = isset($_GET['id']) ? (int)$_GET['id'] : 1;
+
+    // Consultas de la noticia y sus imagenes
+    $sql_noticia = "SELECT * FROM noticias WHERE id = $id";
+    $sql_imagenes = "SELECT ruta FROM imagenes WHERE noticia_id = $id";
+
+    $res = $mysqli->query($sql_noticia);
+    $noticia = $res->fetch_assoc();
+
+    $resImgs = $mysqli->query($sql_imagenes);
+    $imagenes = $resImgs->fetch_all(MYSQLI_ASSOC);
+
+    echo $twig->render('noticia_imprimir.html.twig', [
+        'noticia' => $noticia,
+        'imagenes' => $imagenes
+    ]);
 ?>
